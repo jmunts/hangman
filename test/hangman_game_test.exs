cf36f2f340e7c0f133926281bc747114f1bd3166
John Mark Untalan <jm@aelogica.com>
2017-10-23 07:33:33 +0800

    [DONE] Refactor and Tidy Up

--- test/hangman_game_test.exs
+++ test/hangman_game_test.exs
@@ -15,46 +15,46 @@ defmodule HangmanGameTest do
   test "state isn't changed for :won and :lost game" do
     for state <- [:won, :lost] do
       game = Game.new_game() |> Map.put(:game_state, state)
-      assert { ^game, _ } = Game.make_move(game, "x")
+      assert ^game  = Game.make_move(game, "x")
     end
   end
 
   test "first occurrence of letter is not already used" do
     game = Game.new_game()
-    { game, _tally } = Game.make_move(game, "x")
+    game = Game.make_move(game, "x")
     assert game.game_state != :already_used
   end
 
   test "second occurrence of letter is already used" do
     game = Game.new_game()
-    { game, _tally } = Game.make_move(game, "x")
+    game = Game.make_move(game, "x")
     assert game.game_state != :already_used
-    { game, _tally } = Game.make_move(game, "x")
+    game = Game.make_move(game, "x")
     assert game.game_state == :already_used
   end
 
   test "good guess" do
     game = Game.new_game("test")
-    { game, _tally } = Game.make_move(game, "t")
+    game = Game.make_move(game, "t")
     assert game.game_state == :good_guess
   end
 
   test ":won" do
     game = Game.new_game("test")
-    { game, _tally } = Game.make_move(game, "t")
+    game = Game.make_move(game, "t")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
-    { game, _tally } = Game.make_move(game, "e")
+    game = Game.make_move(game, "e")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
-    { game, _tally } = Game.make_move(game, "s")
+    game = Game.make_move(game, "s")
     assert game.game_state == :won
     assert game.turns_left == 7
   end
 
   test "bad_guess is recognized" do
     game = Game.new_game("test")
-    { game, _tally } = Game.make_move(game, "x")
+    game = Game.make_move(game, "x")
     assert game.game_state == :bad_guess
     assert game.turns_left == 6
   end
@@ -63,25 +63,25 @@ defmodule HangmanGameTest do
     game = Game.new_game("x")
 
     # TODO: Refactor using Enum.reduce
-    { game, _tally } = Game.make_move(game, "a")
+    game = Game.make_move(game, "a")
     assert game.game_state == :bad_guess
     assert game.turns_left == 6
-    { game, _tally } = Game.make_move(game, "b")
+    game = Game.make_move(game, "b")
     assert game.game_state == :bad_guess
     assert game.turns_left == 5
-    { game, _tally } = Game.make_move(game, "c")
+    game = Game.make_move(game, "c")
     assert game.game_state == :bad_guess
     assert game.turns_left == 4
-    { game, _tally } = Game.make_move(game, "d")
+    game = Game.make_move(game, "d")
     assert game.game_state == :bad_guess
     assert game.turns_left == 3
-    { game, _tally } = Game.make_move(game, "e")
+    game = Game.make_move(game, "e")
     assert game.game_state == :bad_guess
     assert game.turns_left == 2
-    { game, _tally } = Game.make_move(game, "f")
+    game = Game.make_move(game, "f")
     assert game.game_state == :bad_guess
     assert game.turns_left == 1
-    { game, _tally } = Game.make_move(game, "g")
+    game = Game.make_move(game, "g")
     assert game.game_state == :lost
   end
 
